e270be3c0af86cbb7fb2531dd6fc4306abf47813
Laura Comar <lautmcomar@gmail.com>
2024-09-14 15:14:00 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,8 @@
+// Task 1: Create an Inventory Array of Product Objects
+let Inventory = [
+    { name: 'Mascara', price: 20, quantity: 20, lowStockLevel: 10 },
+    { name: 'Blush', price: 40, quantity: 30, lowStockLevel: 15 },
+    { name: 'Foundation', price: 70, quantity: 50, lowStockLevel: 25 },
+    { name: 'Eyeliner', price: 15, quantity: 25, lowStockLevel: 8 },
+    { name: 'Lipstick', price: 12, quantity: 40, lowStockLevel: 20 },
+];
