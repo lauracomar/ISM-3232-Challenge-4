25ddbebfa857f16b16489236fcf5297f2b614609
Laura Comar <lautmcomar@gmail.com>
2024-09-14 17:34:56 -0400

    Create updateStock function

--- inventory_management.js
+++ inventory_management.js
@@ -11,4 +11,14 @@ let Inventory = [
 function displayProductDetails(products) {
     let productinstock = products.quantity >= products.lowStockLevel ? "In Stock" : "Low Stock"; //compare quantity and low level stock to see if item is in stock or low in stock
     console.log(`${products.name}, $${products.price}, ${products.quantity},${productinstock}`); // display message with all the products informations + if product is in or low stock
-}; 
+};
+
+// Task 3: Create a Function to Update Product Stock After Sales
+function updateStock(products, unitsSold) {
+    products.quantity -= unitsSold;
+    let productinstock = products.quantity >= products.lowStockLevel ? "In Stock" : "Low Stock"; // update function to see if an item is in or low in stock after a sale
+    console.log(`Updated quantity for product ${products.name} is ${products.quantity}`);
+    if (unitsSold > products.quantity) {
+        return (`Not enough products to sell`) // message in case tries to sell more than what inventory has
+    };
+};
