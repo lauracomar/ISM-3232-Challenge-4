810d30987fed6930e47b97ee0753984991a68401
Laura Comar <lautmcomar@gmail.com>
2024-09-14 15:33:06 -0400

    Create displayProductDetails function

--- inventory_management.js
+++ inventory_management.js
@@ -5,4 +5,10 @@ let Inventory = [
     { name: 'Foundation', price: 70, quantity: 50, lowStockLevel: 25 },
     { name: 'Eyeliner', price: 15, quantity: 25, lowStockLevel: 8 },
     { name: 'Lipstick', price: 12, quantity: 40, lowStockLevel: 20 },
-];
+]; // product informations
+
+// Task 2: Create a Function to Display Product Details
+function displayProductDetails(products) {
+    let productinstock = products.quantity >= products.lowStockLevel ? "In Stock" : "Low Stock"; //compare quantity and low level stock to see if item is in stock or low in stock
+    console.log(`${products.name}, $${products.price}, ${products.quantity},${productinstock}`); // display message with all the products informations + if product is in or low stock
+}; 
